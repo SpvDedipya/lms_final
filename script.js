93def8fc6e73f269356ecc575cd4bd4e77441d7d
GOTETI JAYACHANDRA MOHAN LAKSHMI NARASIMHA MURTHY <63448352+jayachandragoteti@users.noreply.github.com>
2021-08-03 12:55:33 +0530

    ajax added

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 ajaxRegisterPageCall();
 function ajaxRegisterPageCall() {
-    $.ajax({
-        url: './home.html',
-        success: function (response) {
-            $('.ajax-main-content').html(response);
-            // $('.Register').addClass('active');
-            // $(
-            //     '.Home,.MyRequests,.MyRequests,.ChangePassword,.Profile,.Login'
-            // ).removeClass('active');
-        },
-    });
+  $.ajax({
+    url: './home.html',
+    success: function (response) {
+      $('.ajax-main-content').html(response);
+    },
+  });
 }
